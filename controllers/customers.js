e99c9c36ecbc221576faa42bf0ec75e754e46917
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-11 13:28:32 -0700

    create customer and read customer added

--- controllers/customers.js
+++ controllers/customers.js
@@ -3,7 +3,8 @@ const Customer = require('../models/customer');
 module.exports = {
 
     new: newCustomer,
-    create
+    create,
+    index
 }
 
 function newCustomer(req, res) {
@@ -11,16 +12,17 @@ function newCustomer(req, res) {
 
 };
 
+// using the customer model to create the 
+// customer submitted by the form.
 async function create(req, res) {
 
     try {
         await Customer.create(req.body)
 
-        console.log(req.body);
 
    // Next step is to display the customer information
 
-        //res.redirect('/customers')
+        res.redirect('/customers')
 
     } catch (error) {
         res.render('error', { message: 'error', error });
@@ -28,3 +30,16 @@ async function create(req, res) {
 
 
 };
+
+
+// use index function to read and display the documents 
+// from mongoDB
+
+async function index(req, res) {
+    try {
+        const customers = await Customer.find();
+        res.render('customers/index', { customers });
+    } catch (error) {
+        res.render('error', { message: 'Error fetching customers', error });
+    }
+}
