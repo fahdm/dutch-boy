a2652893e61ae9bdea41c4c03cd4b84af49f89ec
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-11 20:53:46 -0700

    refrence products model, validate data types

--- models/customer.js
+++ models/customer.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const productSchema = require('./product')
+
 const customerSchema = new Schema ({
     name: {
         type: String,
@@ -27,8 +29,24 @@ const customerSchema = new Schema ({
         type: String,
         
     },
-    notes: String,
-    dateOfBirth: Date
+    notes:{
+     
+        type:String
+
+    } ,
+
+    dateOfBirth:{
+        type: Date
+    },
+    
+    products:[{
+
+        type: Schema.Types.ObjectId,
+        ref:'Product'
+
+    }],
+
+
 }, {
     timestamps: true
 });
