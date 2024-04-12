15aab1f34050937c23379aa8e46e3208ea3f6f2f
Fahad Mubiru <fahadmubiru1@gmail.com>
2024-04-11 20:50:56 -0700

    add update and delete operations

--- controllers/customers.js
+++ controllers/customers.js
@@ -4,7 +4,11 @@ module.exports = {
 
     new: newCustomer,
     create,
-    index
+    index,
+    delete:deleteCustomer,
+    show,
+    edit,
+    update
 }
 
 function newCustomer(req, res) {
@@ -18,6 +22,8 @@ async function create(req, res) {
 
     try {
         await Customer.create(req.body)
+
+        console.log(req.body);
 
 
    // Next step is to display the customer information
@@ -43,3 +49,47 @@ async function index(req, res) {
         res.render('error', { message: 'Error fetching customers', error });
     }
 }
+
+async function deleteCustomer(req,res){
+    try {
+
+        const customers = await Customer.deleteOne({_id:req.params.id});
+        res.redirect('/customers');
+        
+    } catch (error) {
+        
+        res.render('error', { message: 'Error deleting customers', error });
+    }
+}
+
+async function show(req,res){
+
+    try { 
+
+        const customer = await Customer.findById(req.params.id);
+        res.render('customers/show', {title: 'Customer Details', customer});
+        
+    } catch (error) {
+        
+        res.render('error', { message: 'Cannot find customer', error });
+    }
+
+}
+
+async function edit(req, res) {
+    try {
+      const customer = await Customer.findById(req.params.id);
+      res.render('customers/edit', { title: 'Edit Customer', customer });
+    } catch (error) {
+      res.render('error', { message: 'Cannot find customer', error });
+    }
+  }
+
+  async function update(req, res) {
+    try {
+      await Customer.findByIdAndUpdate(req.params.id, req.body);
+      res.redirect(`/customers/${req.params.id}`);
+    } catch (error) {
+      res.render('error', { message: 'Error updating customer', error });
+    }
+  }
